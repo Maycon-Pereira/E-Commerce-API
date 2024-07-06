c5cd8b27f1b111cc58639351e9e1858171802194
Maycon-Pereira <pmaycon63@gmail.com>
2024-07-05 23:41:06 -0300

    repository cart

--- commerce/src/main/java/com/api/commerce/repository/CarrinhoRepository.java
+++ commerce/src/main/java/com/api/commerce/repository/CarrinhoRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.api.commerce.entity.Carrinho;
@@ -13,7 +14,13 @@ public interface CarrinhoRepository extends JpaRepository<Carrinho, String>{
 
 	Page<Carrinho> findAllByAtivoTrue(Pageable paginacao);
 
-
+	@Query("""
+			SELECT c FROM carrinho c 
+			WHERE 
+			c.user_id = :user_id 
+			AND 
+			c.ativo = true
+			""")
 	Optional<Carrinho> findByUserIdAndAtivoTrue(@Param("user_id") String user_id);
 
 }
